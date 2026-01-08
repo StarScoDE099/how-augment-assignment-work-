13606e3e36438c606a798d36d89841a06c596215
StarScoDE099 <odhiambostanley099@gmail.com>
2026-01-08 22:07:56 +0300

    Add files via upload

---
+++ howaugmentassignmentwork.py
@@ -0,0 +1,72 @@
+#AUGMENTED ASSIGNMENT COMBINES A BINARY OPERATION WITH AN ASSIGNMENT IN ONE STEP
+
+my_var = 10
+my_var += 5
+print(my_var) #15
+
+
+# N/B= ADVANTAGE OF AUGMENTED ASSIGNMENT IT PROVIDES A CONCISE AND READABLE WAY TO UPDATE A VARIABLE
+
+
+#SUBTRACTION ASSIGNMENT (-=)
+
+count = 14
+count -= 3
+print(count) # 11
+
+#MULTIPLICATIO ASSIGNMENT (*=)
+
+product = 65
+product *= 7
+
+print(product) #455
+
+#THE DIVISION ASSIGNMENT (/=)
+price = 100
+price /= 4
+
+print(price)# 25
+
+#THE FLOOR DIVISION(//=)
+total_pages = 23
+total_pages //= 5
+print(total_pages) # 4
+
+#THE MODULUS (%=)
+
+bits = 35
+bits %= 2
+
+print(bits) #1
+
+#THE EXPONENTIATION(**=)
+
+power = 2
+power **= 3
+
+print(power) # 8
+
+
+#CONCATENATE STRING USING AUGMENT ASSIGNMENT
+
+greet ='Hello'
+greet += ' World'
+
+print(greet) #Hello world
+
+#REPEAT STRING USING MULTIPLICATION ASSIGNMENT
+
+greet = 'Hello'
+greet *= 10
+
+print(greet)
+
+#USING UNARY PLUS TWICE (++*) TO INCREMENT ANYTHING
+
+my_var = 5
+
+print(+my_var)
+print(++my_var)
+
+my_var += 1
+print(my_var) # 6s
